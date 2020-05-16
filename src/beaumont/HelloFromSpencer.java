461462b57bf05f549b1642b6884f14776ada0d4a
SELLIS-BYUI <63525626+SELLIS-BYUI@users.noreply.github.com>
2020-05-16 17:15:02 -0600

    commit to spencer

--- src/beaumont/HelloFromSpencer.java
+++ src/beaumont/HelloFromSpencer.java
@@ -4,5 +4,6 @@ public class HelloFromSpencer {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Spencer.");
+	    System.out.println("Hello Spencer, my name is Stephen :D");
     }
 }
